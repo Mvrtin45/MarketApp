2d8ad2c40ec839274561909c186c6af193beeed6
Martin <ma.carriel@duocuc.cl>
2024-10-03 17:25:01 -0300

    Revert "editar perfil listo y modificar contrasena + app funcional en telefono."
    This reverts commit a410d24860c4f4612b4f6d23d8e11b393a7d2d92.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {NativeStorage} from '@awesome-cordova-plugins/native-storage/ngx';
-import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideAnimationsAsync(), NativeStorage, SQLite],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
