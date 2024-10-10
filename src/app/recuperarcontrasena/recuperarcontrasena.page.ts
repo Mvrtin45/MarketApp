56887b7569c55041b6115111c411f8a781bcb414
Martin <ma.carriel@duocuc.cl>
2024-10-10 17:32:31 -0300

    Recuperar contrasena listo

--- src/app/recuperarcontrasena/recuperarcontrasena.page.ts
+++ src/app/recuperarcontrasena/recuperarcontrasena.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NavController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recuperarcontrasena',
@@ -6,10 +8,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recuperarcontrasena.page.scss'],
 })
 export class RecuperarcontrasenaPage implements OnInit {
+  formularioRecuperar: FormGroup;
+  emailErrorMessage: string = '';
+  correo: string = "";
 
-  constructor() { }
+  constructor( private formBuilder: FormBuilder,
+    private navCtrl: NavController,
+    private alertController: AlertController
+  ) {
+    this.formularioRecuperar = this.formBuilder.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ]
+    });
+  }
 
   ngOnInit() {
   }
 
+  async recuperarContrasena() {
+    this.emailErrorMessage = ''; // Reinicia el mensaje de error
+
+    // Validaciones
+    if (!this.correo) {
+      this.emailErrorMessage = 'El correo es obligatorio.';
+      return;
+    }
+
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailPattern.test(this.correo)) {
+      this.emailErrorMessage = 'Por favor, introduce un correo válido.';
+      return;
+    }
+
+    // Aquí puedes implementar la lógica para enviar el correo de recuperación
+
+    // Mostrar la alerta después de la acción
+    const alert = await this.alertController.create({
+      header: 'Correo enviado',
+      message: 'Hemos enviado un enlace de recuperación de contraseña a tu correo.',
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            // Redirigir al login después de hacer clic en OK
+            this.navCtrl.navigateRoot('/login');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
