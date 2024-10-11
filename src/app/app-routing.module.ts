f8dba17be233b28918ccd6f1cc15a51ff27e173c
Martin <ma.carriel@duocuc.cl>
2024-10-10 23:03:02 -0300

    Integración de logo en todas las ventanas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'register',
+    redirectTo: 'ayudasoporte',
     pathMatch: 'full'
   },{
     path: '',
