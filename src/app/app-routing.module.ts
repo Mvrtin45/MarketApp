4834582d989f1751c12e4571d5ff8b2f56ee2a70
Martin <ma.carriel@duocuc.cl>
2024-10-12 23:09:21 -0300

    Ajustes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'ayudasoporte',
+    redirectTo: 'register',
     pathMatch: 'full'
   },{
     path: '',
